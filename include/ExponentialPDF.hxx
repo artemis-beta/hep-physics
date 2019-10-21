1f769f709512a04b67e4707467a6c15de7cb186c
artemis-beta <KXZ186@student.bham.ac.uk>
2019-10-21 19:34:53 +0100

    Added Exponential PDF

---
+++ include/ExponentialPDF.hxx
@@ -0,0 +1,35 @@
+#ifndef __EXPO_HXX__
+#define __EXPO_HXX__
+
+#include "PDF.hxx"
+#include "Constants.hxx"
+#include <cmath>
+
+using namespace PHYS::PDF;
+
+const double _expo_invcdf(const double x, const Params p)
+{
+    return -log(1-x)/p[0];
+}
+
+namespace PHYS
+{
+    namespace PDF
+    {
+        /*! @brief  Exponential Class
+        @details    Class representing the Cauchy/Breit-Wigner PDF
+        @version    0.1.0
+        @author     Kristian Zarebski
+        @date 	    last modified 2019-10-21
+        @copyright  MIT License
+        */
+        class Exponential : public ProbabilityDensityFunction
+        {
+            public:
+                Exponential(const double lambda) :
+                    ProbabilityDensityFunction("Cauchy", {lambda},_expo_invcdf){}
+        };
+    };
+};
+
+#endif
