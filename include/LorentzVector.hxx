a0f5d40d50c06723079aef063eba78a1d7708e90
artemis-beta <KXZ186@student.bham.ac.uk>
2019-10-24 19:44:49 +0100

    Fixed Lorentz Vec bug

--- include/LorentzVector.hxx
+++ include/LorentzVector.hxx
@@ -31,7 +31,7 @@ namespace PHYS
             //! Create a new LorentzVector by giving the four values @f$(x_0, x_1, x_2, x_3)@f$
             LorentzVector(double x0, double x1, double x2, double x3);
             //! Returns the magnitude of the vector as @f$ r = \sqrt{x_0^2-x_1^2-x_2^2-x_3^2} @f$
-            const double magnitude() const {return pow(x0*x0-x1*x1+x2*x2, 0.5);}
+            const double magnitude() const {return pow(x0*x0-x1*x1-x2*x2-x3*x3, 0.5);}
     };
 }
 #endif
