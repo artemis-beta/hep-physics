c4c6a63d7f8942ac373edb33fb25b54a7f871ece
artemis-beta <KXZ186@student.bham.ac.uk>
2019-10-24 19:45:44 +0100

    Updated DataFrame Example

--- examples/DataFrameExample.cxx
+++ examples/DataFrameExample.cxx
@@ -6,6 +6,7 @@ using namespace PHYS::Data;
 int main()
 {
     DataFrame* d = new DataFrame;
-    d->addColumnBool("Test", {0,1});
+    d->addColumnBool("Test", {0,1,0,0,1,0,1});
+    d->addColumnDouble("Test2", {2.3,2.3,3.54,1.3,1.53,6.7,3.4});
     d->head();
 }
